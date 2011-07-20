aeba570d28072cc17c82486dd7b8a7808054cace
Rabin Joshi <rabinjoshi@ymail.com>
2011-07-20 12:59:33 -0500

    Initial Commit

---
+++ LocalGit/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  LocalGit
+//
+//  Created by Rabin Joshi on 7/20/11.
+//  Copyright 2011 Techgene. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
